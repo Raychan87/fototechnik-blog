fe8988e729bdf26f368a35ffefc97ab921811abd
Raychan <davidrueckert@hotmail.de>
2019-10-18 11:21:24 +0200

    first upload

---
+++ fototechnik-blog/index.php
@@ -0,0 +1,24 @@
+
+<!-- Header aufrufen -->
+<?php get_header(); ?> 
+<!-- Start des Contents Container -->
+
+
+
+
+
+<!-- Titel aufrufen -->
+<h1><?php the_title(); ?></h1> 
+
+
+<p><?php the_content(); ?></p>
+
+
+
+
+
+
+<!-- Ende des Contents Container -->
+
+<!-- Footer aufrufen -->
+<?php get_footer(); ?>
