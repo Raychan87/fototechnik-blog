da147d4590f57b9ce3ee3a1cf4cfc3415968fe6d
Raychan <davidrueckert@hotmail.de>
2019-10-20 18:32:46 +0200

    folder fix

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+
+
+
+
+<?php
+
+/*
+<------ Menü anzeigen ------>
+*/
+wp_nav_menu(
+    array(
+        'theme_location' => 'foot_menu',  //-> id des aufzurufendes Menüs
+        'menu_class' => 'meta-nav-list' //-> ein Klassen Name wird hinzugefügt
+        'depth' => 1                        //-> gibt die Tiefe des Menüs an
+    )
+)
+/*
+<------ Ende Menü ------>
+*/
+
+?>
+
+
+
+
+
+</body>
+</html>
