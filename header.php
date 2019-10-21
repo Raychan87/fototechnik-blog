ee119d63791341c9c7d50d1b95f8a5b1eb83ec90
Raychan <davidrueckert@hotmail.de>
2019-10-21 08:14:36 +0200

    Optimierung

--- header.php
+++ header.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-
     <meta charset"UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Der Meta viewport soll immer so groß sein wie das Gerät -->
+    <!-- Der Meta viewport soll immer so groß sein wie das Gerät -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 
     <!-- Browser Titel Namen -->
     <title><?php wp_title('->');?> <?php bloginfo( 'name' ); ?></title> 
     
-
     <!-- CSS Code laden -->
-        <!-- Öffnent Style.css -->
+        <!-- Öffnet Style.css -->
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">  
         <!-- "normalize" für Browser kompatibilität --> 
     <link rel="skylesheet" href="<?php bloginfo('template_url');?>/css/normalize.css">
     <link rel="skylesheet" href="#">
 
-    <?php 
-        wp_head(); //-> Lädt Wordpress eigene Scripte, CSS Code für Emoji und Meta Informationen (Auch die Adminbar und wp_footer() wird benötigt)
-    ?>
+    <!-- Lädt Wordpress eigene Scripte, CSS Code für Emoji, Metainformationen und Adminbar (wp_footer() wird benötigt) -->
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
     <header>
@@ -27,5 +25,6 @@
         <!-- Untertitel bzw. Beschreibung der Webseite -->
         <p><?php bloginfo( 'description' ); ?></a></p>
         
+        <!-- Navigations Bar wird aufgerufen -->
         <?php get_template_part('template_parts/navi');?>
     </header>
