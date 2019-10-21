d4d35a4de78c6a2ec5ef9cb2d49cd14579835f82
Raychan <davidrueckert@hotmail.de>
2019-10-21 09:49:26 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
         <!-- gibt das aktuelle Jahr und den Namen der Webseite aus -->
         <p>Copyright 2018 - <?php echo date('Y'); ?> - <?php bloginfo('name'); ?></p>
     </footer>
+</div> <!-- die Classe "site" wird hier wieder geschlossen -->
 
     <!-- Wird für die Adminleiste geladen (wp_head() wird auch benötigt)) -->
     <?php wp_footer();?> 
