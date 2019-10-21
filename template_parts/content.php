73c7e95789d2940c45cf2c243d6cfdc93d1384b4
Raychan <davidrueckert@hotmail.de>
2019-10-21 08:17:41 +0200

    add Author und Date

--- template_parts/content.php
+++ template_parts/content.php
@@ -2,6 +2,10 @@
 <article <?php post_class();?>>
     <!-- Überschrift des Beitrages -->
     <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+    
+    <!-- Erzeugt Author und Datum des Beitrags -->
+    <p>Veröffentlich von <?php the_author(); ?> am <?php the_time('d.m.Y');?>.</p>
+    
     <!-- Der Inhalt des Beitrages -->
     <?php the_content();?>
 </article>
