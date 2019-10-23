584e7af3a594694b0f29867918f4ab803038bc71
Raychan <davidrueckert@hotmail.de>
2019-10-23 08:44:33 +0200

    Update content-error.php

--- template_parts/content-error.php
+++ template_parts/content-error.php
@@ -1,3 +1,16 @@
-<article>
-    Nichts gefunden.
-</article>
+
+<!-- Wenn man von der Suchfunktion kommt -->
+<?php if(is_search()) { ?>
+
+    <section <?php post_class();?>>
+        <p>Leider wurde nichts gefunden.</p>
+    </section>
+
+<?php } else { ?>
+
+    <section <?php post_class();?>>
+        <h1>Fehler 404</h1>
+        <p>Diese Seite existiert nicht.</p>
+    </section>
+
+<?php } ?>
