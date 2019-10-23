fff163964846adad6f3f0c591c65fcc61f2b0e60
Raychan <davidrueckert@hotmail.de>
2019-10-23 08:34:38 +0200

    fix h1

--- template_parts/content.php
+++ template_parts/content.php
@@ -1,7 +1,7 @@
 <!-- Beitrag -->
 <article <?php post_class();?>>
     <!-- Überschrift des Beitrages -->
-    <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+    <h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
     
     <!-- Erzeugt Author und Datum des Beitrags -->
     <p>Veröffentlich von <?php the_author(); ?> am <?php the_time('d. M Y');?>.</p>
