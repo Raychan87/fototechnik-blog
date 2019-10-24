cbc99efade8ee122e9ba3ce504726d6974b3606b
Raychan <davidrueckert@hotmail.de>
2019-10-24 09:10:06 +0200

    Update content.php

--- template_parts/content.php
+++ template_parts/content.php
@@ -3,11 +3,17 @@
     <!-- Überschrift des Beitrages -->
     <h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
     
+    <!-- Kategorie anzeigen -->
+    <?php the_category(', ');?>
+    
     <!-- Erzeugt Author und Datum des Beitrags -->
     <p>Veröffentlich von <?php the_author(); ?> am <?php the_time('d. M Y');?>.</p>
     
     <!-- Der Inhalt des Beitrages -->
     <?php the_content();?>
+    
+    <!-- Schlagwörter anzeigen -->
+    <?php the_tags();?>
     
     <!-- Beitrags Navigations (vor und zurück) -->
     <?php previous_post_link();?>
