279533e16334c4ed360da2eb297deabc0ce0552c
Raychan <davidrueckert@hotmail.de>
2019-10-24 09:25:05 +0200

    Create catagory.php

---
+++ catagory.php
@@ -0,0 +1,34 @@
+<!-- header.php aufrufen -->
+<?php get_header(); ?> 
+
+<!-- Der Loop wird in ein Container gepackt -->
+<main class="container_main"> 
+    <div class="container_article">
+    
+    <!-- Kategorie als Titel und Beschreibung -->
+    <h1><?php single_cat_title();?></h1>
+    <?php echo category_description();?>
+    
+        <!-- Der Loop läuft nur die Anzahl der angegeben Beiträge in den Einstellungen -->
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                
+                <!-- Ruft die Content.php Datei auf um die Beiträge bzw Seite aufzurufen -->
+                <?php get_template_part('template_parts/content');?>
+            
+            <?php endwhile; else : ?>
+                
+                <!-- Fehlermeldung, es konnten keine Beiträge gefunden werden -->
+                <?php get_template_part('template_parts/content','error');?>
+
+        <?php endif; ?>
+        
+        <!-- Beitrags Navigations (vor und zurück) -->
+        <?php previous_posts_link();?>
+        <?php next_posts_link();?>
+    </div>
+    <!-- sidebar.php aufrufen -->
+    <?php get_sidebar() ;?>
+</main>
+
+<!-- footer.php aufrufen -->
+<?php get_footer(); ?>
