157102d52a8fb805484a15cba9408aee55da812c
Raychan <davidrueckert@hotmail.de>
2019-10-25 11:25:23 +0200

    add navibar_side

--- sidebar.php
+++ sidebar.php
@@ -3,6 +3,8 @@
         
         <!-- Suchfunktion wird inkludiert -->
         <?php get_search_form();?>
-            
+        
+        <!-- Navigationsleiste -->
+        <?php get_template_part('template_parts/navibar_side');?>  
     </div>
 </aside>
