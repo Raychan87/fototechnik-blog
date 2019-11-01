58f77adc81ffe8c9218719d47b0d22ea737cf05e
Raychan <davidrueckert@hotmail.de>
2019-11-01 10:00:27 +0100

    change to NavWalker

--- template_parts/navibar_main.php
+++ template_parts/navibar_main.php
@@ -1,13 +1,14 @@
 <nav class="container_navibar_main">
-    <?php 
-    
-    $args = array(
-        'theme_location' => 'navibar_main', /* Navigationsbereich zuweisen */
-        'depth' => 5, /* maximale Tiefe des Menüs */
-        'container' => ''
-    );
-    
-    wp_nav_menu($args);?>
+    <?php wp_nav_menu( array(
+        'theme_location'  => 'primary',
+        'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+        'container'       => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id'    => 'bs-example-navbar-collapse-1',
+        'menu_class'      => 'navbar-nav mr-auto',
+        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'          => new WP_Bootstrap_Navwalker(),
+    ) );?>
 </nav>
  
  
