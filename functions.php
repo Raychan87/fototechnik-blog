0bb28f86c48535feded9525ef4e7f60a6e2ce3a6
Raychan <davidrueckert@hotmail.de>
2019-11-01 11:54:06 +0100

    add Sicherheitsfunktionen

--- functions.php
+++ functions.php
@@ -77,12 +77,50 @@
         </div>
     <?php }?>
             
-     <!-- Sicherheits Funktionen -->
-     <!-- Wordpress Version ausblenden -->
-     <?php function fototechnikblog_remove_generator(){
+    <!-- Sicherheits Funktionen -->
+    <!-- Wordpress Version ausblenden -->
+    <?php function fototechnikblog_remove_generator(){
         add_filter( 'the_generator', create_function( '$x', 'return;'));
-     }
-     add_action('init','fototechnikblog_remove_generator'); ?>
-     <!-- Login Fehler deaktivieren -->
-     <?php add_filter('login_errors', create_function('$a',"return null;"));
+    }
+    add_action('init','fototechnikblog_remove_generator'); ?>
+    <!-- Login Fehler deaktivieren -->
+    <?php add_filter('login_errors', create_function('$a',"return null;")); ?>
+    <!-- XML-PRC entfernen / deaktivieren -->
+    <?php add_filter( 'xmlrpc_enabled', '__return_false' ); ?>
+    <!-- Hiermit wird die direkte Verbindung von xmlrpc.php und x-pingback entfernt -->
+    <?php function fototechnikblog_remove_x_pingback( $headers ){
+        unset( $headers['X-Pingback'] );
+        return $headers;
+    }
+    add_filter( 'wp_headers', 'fototechnikblog_remove_x_pingback' ); ?>
+    <!-- RSET API Schnittstelle deaktivieren -->
+    <?php add_filter('rest_enabled', '__return_false'); ?>
+    <?php add_filter('rest_jsonp_enabled', '__return_false'); ?>
+    <!-- WP-JSON Schnittstelle deaktivieren -->
+    <?php add_filter('json_enabled', '__return_false'); ?>
+    <?php add_filter('json_jsonp_enabled', '__return_false'); ?>
+    <!-- DNS Prefatch entfernen -->
+    <?php remove_action('wp_head', 'wp_resource_hints', 2); ?>
+    <!-- RSD Verweis entfernen -->
+        <!-- Mit dem RSD Verweis wird eine XML-Datei mit allen Services des WordPress Blogs bereitgestellt. -->
+    <?php remove_action('wp_head', 'rsd_link'); ?>
+    <!-- WLW Manifest entfernen -->
+        <!-- Das WLW Manifest dient als Beschreibung für externe Editoren, um auf den WordPress Blog zugreifen zu können. -->
+    <?php remove_action( 'wp_head', 'wlwmanifest_link' ); ?>
+    <!-- Emoticons deaktivieren -->
+    <?php function disable_emojis() {
+       remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+       remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+       remove_action( 'wp_print_styles', 'print_emoji_styles' );
+       remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+       remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+       remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+       remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+       add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    }
+    add_action( 'init', 'disable_emojis' ); ?>
+    
+            
+    
+            
             
