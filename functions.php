064cdea935998a1eca2320063de74928ed15fef8
Raychan <davidrueckert@hotmail.de>
2019-11-03 21:15:04 +0100

    add cssmenu and fixes

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
     <!-- Sicherheits Funktion -->
-    <?php function fototechnikblog_safety_function(){
+    <?php function fototechnik_blog_safety_function(){
         add_filter('the_generator', create_function( '$x', 'return;'));  /* Wordpress Version ausblenden */
         add_filter('login_errors', create_function('$a',"return null;")); /* Login Fehler deaktivieren */
         add_filter('xmlrpc_enabled', '__return_false' ); /* XML-RPC Schnittstelle deaktivieren */
@@ -21,54 +21,105 @@
         remove_filter('wp_mail', 'wp_staticize_emoji_for_email' ); /* Emoticons deaktivieren */
         add_filter('tiny_mce_plugins', 'disable_emojis_tinymce' ); /* Emoticons deaktivieren */ 
     } 
-    add_action('init','fototechnikblog_safety_function'); ?>
+    add_action('init','fototechnik_blog_safety_function'); ?>
+
     <!-- Hiermit wird die direkte Verbindung von xmlrpc.php und x-pingback entfernt -->
-    <?php function fototechnikblog_remove_x_pingback( $headers ){
+    <?php function fototechnik_blog_remove_x_pingback( $headers ){
         unset( $headers['X-Pingback'] );
         return $headers;
     }
-    add_filter( 'wp_headers', 'fototechnikblog_remove_x_pingback' ); ?>
+    add_filter( 'wp_headers', 'fototechnik_blog_remove_x_pingback' ); ?>
                
     <!-- HTML5 Converter -->
-<<<<<<< HEAD
-    <?php 
-        $args = array(
-            'search-from',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption'
-        );
-        add_theme_support ('html5', $args);
-    ?> 
+    <?php add_theme_support ('html5', array('search-from','comment-form','comment-list','gallery','caption'));?> 
 
-=======
-    <?php $args = array(
-        'search-from',
-        'comment-form'.
-        'comment-list',
-        'gallery',
-        'caption'
-    };
-    add_theme_support ('html5', $args);?> 
-   
->>>>>>> bfb975a05e3c3ead8c997fa48cd119fe3c30ed78
-    <!-- Navigations Menü -->
-    <!-- Bootstrap Nav Walker wird geladen -->
-    <?php function fototechnikblog_register_navwalker() {
-    	require_once get_template_directory() . '/assets/lib/class-wp-bootstrap-navwalker.php';
-    }
-    add_action( 'after_setup_theme', 'fototechnikblog_register_navwalker' ); ?>
-    <!-- Das Nav Walker Menü registrieren -->
-    <?php register_nav_menus( array(
-	    'primary' => __( 'Primary Menu', 'fototechnik-blog' ),
-    ) ); ?>
 
-    <!-- Navigation Funktion wird aufgerufen -->
-    <?php function fototechnikblog_register_navibar() {
-      register_nav_menu('navibar_main','Navigation in der Kopfzeile'); 
-    }
-    add_action( 'after_setup_theme', 'fototechnikblog_register_navibar' ); ?>
+
+
+
+
+
+<!-- http://cssmenumaker.com/blog/wordpress-3-drop-down-menu-tutorial/ -->
+
+
+
+    <?php add_action('wp_enqueue_scripts', 'cssmenumaker_scripts_styles' );
+
+    function cssmenumaker_scripts_styles() {  
+        wp_enqueue_style( 'cssmenu-styles', get_template_directory_uri() . '/assets/css/cssmenu_styles.css');
+    } ?>
+
+<?php
+class CSS_Menu_Maker_Walker extends Walker {
+
+var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
+function start_lvl( &$output, $depth = 0, $args = array() ) {
+  $indent = str_repeat("\t", $depth);
+  $output .= "\n$indent<ul>\n";
+}
+
+function end_lvl( &$output, $depth = 0, $args = array() ) {
+  $indent = str_repeat("\t", $depth);
+  $output .= "$indent</ul>\n";
+}
+
+function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+  global $wp_query;
+  $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+  $class_names = $value = '';        
+  $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+  /* Add active class */
+  if(in_array('current-menu-item', $classes)) {
+    $classes[] = 'active';
+    unset($classes['current-menu-item']);
+  }
+
+  /* Check for children */
+  $children = get_posts(array('post_type' => 'nav_menu_item', 'nopaging' => true, 'numberposts' => 1, 'meta_key' => '_menu_item_menu_item_parent', 'meta_value' => $item->ID));
+  if (!empty($children)) {
+    $classes[] = 'has-sub';
+  }
+
+  $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+  $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+  $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+  $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+  $output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+  $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+  $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+  $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+  $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+  $item_output = $args->before;
+  $item_output .= '<a'. $attributes .'><span>';
+  $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+  $item_output .= '</span></a>';
+  $item_output .= $args->after;
+
+  $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+}
+
+function end_el( &$output, $item, $depth = 0, $args = array() ) {
+  $output .= "</li>\n";
+}
+}?>
+
+
+
+
+
+
+
+
+
+
+
 
     <!-- Aktivierung der Beitragsformate -->
     <!-- https://wordpress.org/support/article/post-formats/ -->
@@ -80,7 +131,7 @@
     
     <!-- Widgets Initialisierung -->
     <!-- Widgets in die Seitenleiste einbinden -->
-    <?php function fototechnikblog_widgets_inits() {
+    <?php function fototechnik_blog_widgets_inits() {
       // Seitenleiste Rechts
       register_sidebar( 
         array(
@@ -104,11 +155,11 @@
           )
       );
     } 
-    add_action( 'widgets_init','fototechnikblog_widgets_inits'); ?>
+    add_action( 'widgets_init','fototechnik_blog_widgets_inits'); ?>
 
     <!-- Kommentar Funktion -->   
     <!-- Die Funktion für einen einzelnen Kommentar -->
-    <?php function fototechnikblog_comments( $comment, $args, $depth ) { 
+    <?php function fototechnik_blog_comments( $comment, $args, $depth ) { 
         $GLOBALS['comment'] = $comment; ?>
 
         <li class="single-comment">
--- template_parts/navibar_main.php
+++ template_parts/navibar_main.php
@@ -1,16 +1,11 @@
-<nav class="container_navibar_main">
-    <?php wp_nav_menu( array(
-        'theme_location'  => 'primary',
-        'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
-        'container'       => 'div',
-        'container_class' => 'collapse navbar-collapse',
-        'container_id'    => 'bs-example-navbar-collapse-1',
-        'menu_class'      => 'navbar-nav mr-auto',
-        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-        'walker'          => new WP_Bootstrap_Navwalker(),
-    ) );?>
+<nav class="container_navbar" role="navigation">
+  <div class="container">
+  <?php  
+wp_nav_menu(array(  
+  'menu' => 'Main Navigation', 
+  'container_id' => 'cssmenu', 
+  'walker' => new CSS_Menu_Maker_Walker()
+)); 
+?>
+    </div>
 </nav>
- 
- 
- 
- 
