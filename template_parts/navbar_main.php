eb127c870dc7e869be55f14b4966b5f79c44dca8
Raychan <davidrueckert@hotmail.de>
2019-11-04 20:46:04 +0100

    build Navigationsleiste

--- template_parts/navbar_main.php
+++ template_parts/navbar_main.php
@@ -1,6 +1,12 @@
-<nav class="container_navbar">
-  <?php wp_nav_menu( array( 
-    'theme_location' => 'navbar',
-    'container_class' => 'my_extra_menu_class' 
-  ) ); ?>
-</nav>
+<div class="container_navbar">
+  <!-- Fügt eine Checkbox und ein Navigations Button für den Smartphone Modus -->
+  <input type="checkbox" id="responsive-nav"> 
+  <label for="responsive-nav" class="responsive-nav-label"><span>&#9776;</span></label>
+  <!-- Das Menü fängt hier an -->
+  <nav  class="container_main_menu">
+    <?php wp_nav_menu( array( 
+      'theme_location' => 'navbar',
+      'container-id' => ''
+    ) ); ?>
+  </nav>
+</div>
