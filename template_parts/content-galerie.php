a520c4450934dd026de3af1a085dad4f229c6e8c
Raychan <davidrueckert@hotmail.de>
2020-03-11 08:46:58 +0100

    change galerie

--- template_parts/content-galerie.php
+++ template_parts/content-galerie.php
@@ -1,45 +1,18 @@
 <?php
-/* Inhalt eines einzelnen Beitrag, wo mehrere Beitrage aufgerufen werden */?>
+/* Inhalt eines ...*/?>
 <article <?php post_class();?>>
     <div class="content-post">
-        <div class="content-title">
-            <?php /* Wenn eine Kategorie oder Schlagwort Seite aufgerufen wird */
-            if (is_archive()) { 
-                /* Überschrift des Beitrages */
-                ?><h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2><?php
-            } else {
-                /* Überschrift des Beitrages */
-                ?><h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1><?php
-            }
+        <div class="content-title"><?php 
+
+            /* Überschrift des Beitrages */
+            ?><h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1><?php
         ?></div><?php
-        /* Kategorie anzeigen */
-        $categories_list = get_the_category_list(', ');
-        if ( $categories_list ) {
-            echo '<div class="content-categories">' . wp_kses_data( $categories_list ) . '</div>';
-        }
 
         if (has_post_thumbnail()) {
             ?><div class ="content-thumb"><?php
                 /* Beitragsbild anzeigen */
-                the_post_thumbnail('fototechnik-blog-post-900');
+                the_post_thumbnail('fototechnik-blog-post-nav');
             ?></div><?php
-        }
-
-        ?><div class="content-text"><?php
-            /* Der Inhalt des Beitrages */
-            the_excerpt(); /* Übergabe Text für gekürtze Beiträge */
-        ?></div>
-
-        <div class="content-date">
-            <?php /* Erzeugt Author und Datum des Beitrags */ ?>
-            <p>Von <span class="content-author"><?php the_author(); ?></span> <?php the_time('d. M Y');?>.</p>
-        </div>
-
-        <div class="content-readmore" >
-            <?php /* Erzeugt den 'Weiterlesen...'-Button */ ?>
-            <a href="<?php the_permalink(); ?>">
-			    Weiterlesen...
-            </a>
-        </div>
+        }?>
     </div>
 </article>
