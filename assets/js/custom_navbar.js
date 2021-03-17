60837ba7c6952c4050f0ca6363beb45fed88b0c3
Raychan <davidrueckert@hotmail.de>
2021-03-17 21:59:43 +0100

    fix Mobile Version

--- assets/js/custom_navbar.js
+++ assets/js/custom_navbar.js
@@ -13,21 +13,29 @@ jQuery(document).ready(function() {
   });
 });*/
 
+// Überprüft ob der Benutzer im Mobile oder Desktop Modus ist
+const smallDevice = window.matchMedia("(min-width: 768px)");
+smallDevice.addListener(handleDeviceChange);
 
-// Wenn der Benutzer die Seite Scrollt, wird die Funktion ausgeführt
-window.onscroll = function() {fototechnik_blog_navbar_scroll()};
+function handleDeviceChange(e) {
+  if (e.matches){
+    
+    // Wenn der Benutzer die Seite Scrollt, wird die Funktion ausgeführt
+    window.onscroll = function() {fototechnik_blog_navbar_scroll()};
 
-// Läd die ID vom Navbar Container
-var header = document.getElementById("fototechnik-blog-navbar");
+    // Läd die ID vom Navbar Container
+    var header = document.getElementById("fototechnik-blog-navbar");
 
-// Holt sich die Offset Position der Navbar
-var sticky = header.offsetTop;
+    // Holt sich die Offset Position der Navbar
+    var sticky = header.offsetTop;
 
-// Fügt ein Sticky Container hinzu, wenn die ID vom Navbar Container erreicht ist. Bzw. entfernt diesen auch wieder
-function fototechnik_blog_navbar_scroll() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-} 
+    // Fügt ein Sticky Container hinzu, wenn die ID vom Navbar Container erreicht ist. Bzw. entfernt diesen auch wieder
+    function fototechnik_blog_navbar_scroll() {
+      if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+  } 
+}
