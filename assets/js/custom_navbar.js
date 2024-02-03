47ca0012afd6b3c7c0906ca8c7105f0020cc51d1
Raychan <davidrueckert@hotmail.de>
2024-02-03 14:55:28 +0100

    fix sticky navbar

--- assets/js/custom_navbar.js
+++ assets/js/custom_navbar.js
@@ -13,31 +13,40 @@ jQuery(document).ready(function() {
   });
 });*/
 
-// Überprüft ob der Benutzer im Mobile oder Desktop Modus ist
-const smallDevice = window.matchMedia("(min-width: 768px)");
-smallDevice.addListener(handleDeviceChange);
-
-function handleDeviceChange(e) {
-  if (e.matches){
+//Funktion für das Anhaengen des Navmenue am Bildschirmrand
+function sticky_navmenu(x) {
+  if (x.matches){
     // Wenn der Benutzer die Seite Scrollt, wird die Funktion ausgeführt
     window.onscroll = function() {fototechnik_blog_navbar_scroll()};
 
     // Läd die ID vom Navbar Container
-    var header = document.getElementById("fototechnik-blog-navbar");
+    var navbar = document.getElementById("fototechnik-blog-navbar");
 
     // Holt sich die Offset Position der Navbar
-    var sticky = header.offsetTop;
+    var sticky = navbar.offsetTop;
 
     // Fügt ein Sticky Container hinzu, wenn die ID vom Navbar Container erreicht ist. Bzw. entfernt diesen auch wieder
     function fototechnik_blog_navbar_scroll() {
       if (window.pageYOffset >= sticky) {
-        header.classList.add("sticky");
+        navbar.classList.add("sticky")
       } else {
-        header.classList.remove("sticky");
+        navbar.classList.remove("sticky");
       }
     }
   } 
 }
+
+// Überprüft ob der Nutzer an einen großen Bildschirm sitzt
+// Create a MediaQueryList object
+const BigDevice = window.matchMedia("(min-width: 768px)");
+
+// Call the match function at run time
+sticky_navmenu(BigDevice);
+
+// Add the match function as a listener for state changes
+BigDevice.addEventListener("change", function(){
+  sticky_navmenu(BigDevice);
+});
 
 
 
