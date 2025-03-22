35da794a73750b40c45bdf6b499a04e06a401f1d
Raychan <davidrueckert@hotmail.de>
2025-03-22 10:25:26 +0100

    fix castle

--- template_parts/content-taxonomy-castle.php
+++ template_parts/content-taxonomy-castle.php
@@ -4,15 +4,9 @@
     if (!has_post_format('image')) {
     ?><div class="content-post-castle">
         <div class="content-title-castle">
-            <?php /* Wenn eine Kategorie oder Schlagwort Seite aufgerufen wird */
-            if (is_archive()) { 
-                /* Überschrift des Beitrages */
-                ?><h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2><?php
-            } else {
-                /* Überschrift des Beitrages */
-                ?><h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1><?php
-            }
-        ?></div><?php
+            <?php /* Überschrift des Beitrages */ ?>
+            <a href="<?php the_permalink();?>"><?php the_title();?></a>
+        </div><?php
 
         if (has_post_thumbnail()) { /* Beitragsbild anzeigen */
             ?><div class ="content-thumb-castle">
